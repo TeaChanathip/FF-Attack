a9eb5674a8e10f61d80ac9868707a0b0d8b6330e
TeaChanathip <teachanthip@gmail.com>
2024-10-20 03:04:48 +0700

    update test-clflush

--- src/test-clflush.c
+++ src/test-clflush.c
@@ -27,11 +27,11 @@ void dummy()
     x++;
 }
 
-// Function to compare two unsigned long long integers for qsort
+// Function to compare two integers for qsort
 int compare_ull(const void *a, const void *b)
 {
-    unsigned long long int_a = *(unsigned long long *)a;
-    unsigned long long int_b = *(unsigned long long *)b;
+    int32_t int_a = *(int32_t *)a;
+    int32_t int_b = *(int32_t *)b;
     if (int_a < int_b)
         return -1;
     if (int_a > int_b)
@@ -39,7 +39,7 @@ int compare_ull(const void *a, const void *b)
     return 0;
 }
 
-unsigned long long median(unsigned long long *sorted_arr, int n)
+int32_t median(int32_t *sorted_arr, int n)
 {
     if (n % 2 == 0)
     {
@@ -51,9 +51,9 @@ unsigned long long median(unsigned long long *sorted_arr, int n)
     }
 }
 
-double variance(unsigned long long *arr, int n)
+double variance(int32_t *arr, int n)
 {
-    unsigned long long sum = 0;
+    int32_t sum = 0;
     for (int i = 0; i < n; i++)
     {
         sum += arr[i];
@@ -68,9 +68,9 @@ double variance(unsigned long long *arr, int n)
     return variance_sum / n;
 }
 
-unsigned long long mean(unsigned long long *arr, int n)
+int32_t mean(int32_t *arr, int n)
 {
-    unsigned long long avg = 0;
+    int32_t avg = 0;
     for (int i = 0; i < n; i++)
     {
         avg += arr[i];
@@ -81,53 +81,66 @@ unsigned long long mean(unsigned long long *arr, int n)
 volatile void perform_test(int n)
 {
     // Allocate memory for results
-    unsigned long long *hit_results = malloc(n * sizeof(unsigned long long));
-    unsigned long long *miss_results = malloc(n * sizeof(unsigned long long));
+    int32_t *hit_results = malloc(n * sizeof(int32_t));
+    int32_t *miss_results = malloc(n * sizeof(int32_t));
 
     // Get the address of the dummy function
     void (*dummy_ptr)() = dummy;
 
     unsigned int aux;
-    unsigned long long start, stop;
     for (int i = 0; i < n; i++)
     {
         delayloop(1000);
         dummy(); // Make sure that dummy will be in the cache
 
         // Test hit
-        start = __rdtscp(&aux);
-        _mm_clflush(dummy_ptr);
-        _mm_mfence();
-        stop = __rdtscp(&aux);
-        hit_results[i] = stop - start;
+        asm volatile(
+            "mfence\n"
+            "lfence\n"
+            "rdtscp\n"
+            "mov %%eax, %%esi\n"
+            "clflush (%1)\n"
+            "mfence\n"
+            "rdtscp\n"
+            "lfence\n"
+            "sub %%esi, %%eax\n"
+            : "=&a" (hit_results[i]): "r" (dummy_ptr): "ecx", "edx", "esi"
+        );
 
         // Make sure that dummy will NOT be in the cache
         _mm_clflush(dummy_ptr);
         _mm_mfence();
 
         // Test miss
-        start = __rdtscp(&aux);
-        _mm_clflush(dummy_ptr);
-        _mm_mfence();
-        stop = __rdtscp(&aux);
-        miss_results[i] = stop - start;
+        asm volatile(
+            "mfence\n"
+            "lfence\n"
+            "rdtscp\n"
+            "mov %%eax, %%esi\n"
+            "clflush (%1)\n"
+            "mfence\n"
+            "rdtscp\n"
+            "lfence\n"
+            "sub %%esi, %%eax\n"
+            : "=&a" (miss_results[i]): "r" (dummy_ptr): "ecx", "edx", "esi"
+        );
     }
 
     // Caculate Min, Max, Mean, Median, Variance of each result arrays
     printf("====== Hit ======\n");
-    qsort(hit_results, n, sizeof(unsigned long long), compare_ull);
-    printf("Min: %llu\n", hit_results[0]);
-    printf("Max: %llu\n", hit_results[n - 1]);
-    printf("Mean: %llu\n", mean(hit_results, n));
-    printf("Median: %llu\n", median(hit_results, n));
+    qsort(hit_results, n, sizeof(int32_t), compare_ull);
+    printf("Min: %u\n", hit_results[0]);
+    printf("Max: %u\n", hit_results[n - 1]);
+    printf("Mean: %u\n", mean(hit_results, n));
+    printf("Median: %u\n", median(hit_results, n));
     printf("Variance: %f\n", variance(hit_results, n));
 
     printf("\n====== Miss ======\n");
-    qsort(miss_results, n, sizeof(unsigned long long), compare_ull);
-    printf("Min: %llu\n", miss_results[0]);
-    printf("Max: %llu\n", miss_results[n - 1]);
-    printf("Mean: %llu\n", mean(miss_results, n));
-    printf("Median: %llu\n", median(miss_results, n));
+    qsort(miss_results, n, sizeof(int32_t), compare_ull);
+    printf("Min: %u\n", miss_results[0]);
+    printf("Max: %u\n", miss_results[n - 1]);
+    printf("Mean: %u\n", mean(miss_results, n));
+    printf("Median: %u\n", median(miss_results, n));
     printf("Variance: %f\n", variance(miss_results, n));
 
     // Free results

59f9c12d71e2ed96ca0e170cd06a790aa11f157d
TeaChanathip <teachanthip@gmail.com>
2024-10-20 02:15:45 +0700

    update test-clflush

--- src/test-clflush.c
+++ src/test-clflush.c
@@ -27,6 +27,47 @@ void dummy()
     x++;
 }
 
+// Function to compare two unsigned long long integers for qsort
+int compare_ull(const void *a, const void *b)
+{
+    unsigned long long int_a = *(unsigned long long *)a;
+    unsigned long long int_b = *(unsigned long long *)b;
+    if (int_a < int_b)
+        return -1;
+    if (int_a > int_b)
+        return 1;
+    return 0;
+}
+
+unsigned long long median(unsigned long long *sorted_arr, int n)
+{
+    if (n % 2 == 0)
+    {
+        return (sorted_arr[n / 2 - 1] + sorted_arr[n / 2]) / 2;
+    }
+    else
+    {
+        return sorted_arr[n / 2];
+    }
+}
+
+double variance(unsigned long long *arr, int n)
+{
+    unsigned long long sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        sum += arr[i];
+    }
+    double mean = (double)sum / n;
+
+    double variance_sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        variance_sum += ((double)arr[i] - mean) * ((double)arr[i] - mean);
+    }
+    return variance_sum / n;
+}
+
 unsigned long long mean(unsigned long long *arr, int n)
 {
     unsigned long long avg = 0;
@@ -37,44 +78,61 @@ unsigned long long mean(unsigned long long *arr, int n)
     return avg / n;
 }
 
-void test_miss(void *dummy_ptr, int n)
+volatile void perform_test(int n)
 {
-    unsigned long long results[n];
-    unsigned int aux;
+    // Allocate memory for results
+    unsigned long long *hit_results = malloc(n * sizeof(unsigned long long));
+    unsigned long long *miss_results = malloc(n * sizeof(unsigned long long));
 
-    _mm_clflush(dummy_ptr);
-    _mm_mfence();
+    // Get the address of the dummy function
+    void (*dummy_ptr)() = dummy;
+
+    unsigned int aux;
+    unsigned long long start, stop;
     for (int i = 0; i < n; i++)
     {
         delayloop(1000);
-        unsigned long long start = __rdtscp(&aux);
+        dummy(); // Make sure that dummy will be in the cache
+
+        // Test hit
+        start = __rdtscp(&aux);
         _mm_clflush(dummy_ptr);
         _mm_mfence();
-        unsigned long long stop = __rdtscp(&aux);
-        results[i] = stop - start;
-    }
+        stop = __rdtscp(&aux);
+        hit_results[i] = stop - start;
 
-    printf("Mean Miss: %llu\n", mean(results, n));
-}
-
-void test_hit(void *dummy_ptr, int n)
-{
-    unsigned long long results[n];
-    unsigned int aux;
-
-    for (int i = 0; i < n; i++)
-    {
-        delayloop(1000);
-        dummy();
-        _mm_mfence();
-        unsigned long long start = __rdtscp(&aux);
+        // Make sure that dummy will NOT be in the cache
         _mm_clflush(dummy_ptr);
         _mm_mfence();
-        unsigned long long stop = __rdtscp(&aux);
-        results[i] = stop - start;
+
+        // Test miss
+        start = __rdtscp(&aux);
+        _mm_clflush(dummy_ptr);
+        _mm_mfence();
+        stop = __rdtscp(&aux);
+        miss_results[i] = stop - start;
     }
 
-    printf("Mean Hit: %llu\n", mean(results, n));
+    // Caculate Min, Max, Mean, Median, Variance of each result arrays
+    printf("====== Hit ======\n");
+    qsort(hit_results, n, sizeof(unsigned long long), compare_ull);
+    printf("Min: %llu\n", hit_results[0]);
+    printf("Max: %llu\n", hit_results[n - 1]);
+    printf("Mean: %llu\n", mean(hit_results, n));
+    printf("Median: %llu\n", median(hit_results, n));
+    printf("Variance: %f\n", variance(hit_results, n));
+
+    printf("\n====== Miss ======\n");
+    qsort(miss_results, n, sizeof(unsigned long long), compare_ull);
+    printf("Min: %llu\n", miss_results[0]);
+    printf("Max: %llu\n", miss_results[n - 1]);
+    printf("Mean: %llu\n", mean(miss_results, n));
+    printf("Median: %llu\n", median(miss_results, n));
+    printf("Variance: %f\n", variance(miss_results, n));
+
+    // Free results
+    free(hit_results);
+    free(miss_results);
 }
 
 int main(int argc, char *argv[])
@@ -87,11 +145,7 @@ int main(int argc, char *argv[])
 
     int n = atoi(argv[1]);
 
-    // Get the address of the dummy function
-    void (*dummy_ptr)() = dummy;
-
-    test_miss(dummy_ptr, n);
-    test_hit(dummy_ptr, n);
+    perform_test(n);
 
     return 0;
 }
